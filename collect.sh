6c30a75b6d87e186b6959b5f601d87e1cf62aa08
XiandongQI <xqiad@connect.ust.hk>
2023-11-10 10:55:14 +0800

    change sub channels to save the Memory

--- collect.sh
+++ collect.sh
@@ -9,4 +9,7 @@ fi
 rm nohup.out
 nohup python3 collect/main.py $1 $2 $3 &
 
+# 难点： 很可能 book snapshot 对不上！需要分开 coin 逐个尝试！
+# bash collect.sh binancefutures btcusdt /home/ubuntu/collect-binancefutures/data
+# bash collect.sh binancefutures ethusdt /home/ubuntu/collect-binancefutures/data
 # sudo pkill -f collect/main.py
